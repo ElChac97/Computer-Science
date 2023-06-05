99f9ea16a7e31c3146c99b296eeb91ebea2c862d
Gerardo Chacón <gchaconalvarez@hotmail.com>
2023-06-05 16:37:52 -0600

    Quitando espacios que me daban TOC

--- Par-Impar.js
+++ Par-Impar.js
@@ -1,6 +1,6 @@
-let numero = prompt('Ingresa un número para saber si es par o impar')
-    if (numero % 2 === 0)
-    alert( numero + ' Es par')
-    else
-    alert(numero + ' Es impar')
+let numero = prompt('Ingresa un número para saber si es par o impar');
+    if (numero % 2 === 0){
+    alert( numero + ' Es par');
+    } else
+    alert(numero + ' Es impar');
 
