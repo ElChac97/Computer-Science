0621987d57f7aeb2f6e37178c4c4d530ee8c2e35
Gerardo Chacón <gchaconalvarez@hotmail.com>
2023-06-05 16:24:13 -0600

    Ejercicio clase 2 terminado

---
+++ Par-Impar.js
@@ -0,0 +1,5 @@
+let numero = prompt('Ingresa un número para saber si es par o impar')
+    if (numero % 2 === 0)
+    console.log( numero + ' Es par')
+    else
+    console.log(numero + ' Es impar')
