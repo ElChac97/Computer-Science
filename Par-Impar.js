ddf60e9c201b4eaa2e652ae51db42d48140e012c
Gerardo Chacón <gchaconalvarez@hotmail.com>
2023-06-06 13:00:00 -0600

    Quitando opción de poner una letra

--- Par-Impar.js
+++ Par-Impar.js
@@ -1,6 +1,8 @@
-let numero = prompt('Ingresa un número para saber si es par o impar');
+let numero = prompt('Ingresa un número para saber si es par o impar'); 
     if (numero % 2 === 0){
     alert( numero + ' Es par');
-    } else
+    } else if (typeof numero === 'string') 
+            alert('Carácter inválido');
+       else
     alert(numero + ' Es impar');
-
+    
